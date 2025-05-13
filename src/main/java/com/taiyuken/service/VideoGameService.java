1ca9b1355104712f8cc0eb031ea57987f996c496
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2025-05-12 20:04:21 -0600

    feat: POST endpoint support for VideoGames Controller

--- src/main/java/com/taiyuken/service/VideoGameService.java
+++ src/main/java/com/taiyuken/service/VideoGameService.java
@@ -1,22 +1,24 @@
 package com.taiyuken.service;
 
+import com.taiyuken.dto.videogame.CreateVideoGameRequest;
 import com.taiyuken.exception.VideoGameNotFoundException;
 import com.taiyuken.model.VideoGame;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class VideoGameService {
     //Mock
-    private static final List<VideoGame> videoGames = List.of(
+    private final List<VideoGame> videoGames = new ArrayList<>(List.of(
             new VideoGame(1, "Stardew valley","Plantar plantitas plantosas"),
             new VideoGame(2, "Left 4 dead","matar zombies y a tus compis"),
             new VideoGame(3, "Fortnite","Esconderte y ser gay si no sacas tus kills"),
             new VideoGame(4, "Rocket League","What a save"),
             new VideoGame(5,"Gang Beasts","Perrazos en 3D")
-    );
+    ));
 
     public List<VideoGame>getVideoGames(){
         return videoGames;
@@ -39,5 +41,12 @@ public class VideoGameService {
         }
         return Optional.empty();
         //return videoGames.stream().filter(videoGame -> videoGame.getId()==id).findFirst();
+    }
+
+    public VideoGame createVideoGame(CreateVideoGameRequest request){
+        int id = videoGames.size()+1;
+        VideoGame videoGame = new VideoGame(id,request.getTitle(),request.getDescription());
+        videoGames.add(videoGame);
+        return videoGame;
     }
 }
